c1c4c84aa64f391c57e224a82d77f16ae2eebf16
bulong <wukunpeng@deepexi.com>
2021-01-26 17:04:54 +0800

    modify scripts

--- conn_node.sh
+++ conn_node.sh
@@ -6,6 +6,7 @@ if [[ -z ${new_node_name} ]]; then
  echo Node Name is needed ~~~
  exit
 fi
+node_port=`cat init.lst|grep -w ${new_node_name}|awk '{print $1}'`
 node_name=`cat init.lst|grep -w ${new_node_name}|awk '{print $2}'`
 if [[ ${node_name} = ${new_node_name} ]];then
  ${base_dir}/bin/mysql -P${node_port} -S ${base_data_dir}/${node_name}/${node_name}.sock 
